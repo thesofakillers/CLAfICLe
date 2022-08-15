75f499e7d66644cb92e1781511bc31bdfba7ca7a
Giulio Starace <giulio.starace@gmail.com>
2022-08-15 19:52:31 +0200

    manually track bigbench
    since google employees arent paid enough to just fkn release it on pypi
    like everyone else

--- gen_pip_reqs.sh
+++ gen_pip_reqs.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 poetry export --without-hashes -o requirements.txt
+echo "# bigbench" >> requirements.txt
+echo "bigbench@https://storage.googleapis.com/public_research_data/bigbench/bigbench-0.0.1.tar.gz" >> requirements.txt
 echo "# local package" >> requirements.txt
 echo "-e ." >> requirements.txt
