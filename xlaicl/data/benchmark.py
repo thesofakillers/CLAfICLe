c33d75bcdb6f7e76a1780809f1f0203a4b180421
Giulio Starace <giulio.starace@gmail.com>
2022-09-16 14:54:43 +0200

    basic implementation of setup and other methods; minor renames

--- xlaicl/data/benchmark.py
+++ xlaicl/data/benchmark.py
@@ -3,7 +3,7 @@ from multiprocessing.dummy import Pool as ThreadPool  # multithreading for IO op
 from multiprocessing import cpu_count
 from typing import Any, Callable, Dict, Iterable, List, Optional
 
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, SequentialSampler
 import pytorch_lightning as pl
 from omegaconf import DictConfig
 import datasets
@@ -14,38 +14,59 @@ import xlaicl.data.process as process
 class BenchmarkDataModule(pl.LightningDataModule):
     def __init__(self, config: DictConfig, lang: str):
         super().__init__()
-        self.config = config
+        self.cfg = config
         self.lang = lang
-        self.raw_save_path: str = os.path.join(self.config.data_dir, "raw")
-        self._process_by_name: Dict[str, Callable[[Any, str], Iterable]] = {
+        self.raw_save_path: str = os.path.join(self.cfg.data_dir, "raw")
+        self._process_by_name: Dict[str, Callable[[Any, str, str], Iterable]] = {
             "xglue;qam": process.xglue
         }
+        self._metadata = {"lang": self.lang, "datasets": []}
 
     def prepare_data(self):
         """takes care of downloading data"""
         # make use of multithreading to speed up by downloading in parallel
         thread_pool = ThreadPool(cpu_count())
-        thread_pool.map(self._download_dataset, self.config.dataset_names)
+        thread_pool.map(self._download_dataset, self.cfg.dataset_names)
 
     def setup(self, stage: Optional[str] = None):
         """
         Gets relevant test split
         Generates k-shot context
-        prepend each input with k-shot context
-        add options column to track options
+        Prepends each input with k-shot context
+        Adds options column to track options
         keep track of metadata throughout
         """
-        for dataset_name in self.config.dataset_names:
+        self._processed_datasets = []
+        for dataset_name in self.cfg.dataset_names:
             dataset = self._load_dataset(dataset_name)
-            self._process_by_name[dataset_name](dataset, self.lang)
+            test_dataset, metrics = self._process_by_name[dataset_name](
+                dataset, self.lang, self.cfg.data_dir
+            )
+            # map dataset idx to name and metrics, so we can track in LightningModule
+            self._metadata["datasets"].append(
+                {"name": dataset_name, "metrics": metrics}
+            )
+            self._processed_datasets.append(test_dataset)
+        # sanity check, failing it should raise some red flags
+        assert (
+            len(self.cfg.dataset_names)
+            == len(self._processed_datasets)
+            == len(self._metadata["datasets"])
+        ), "Mismatch in number of requested, processed, and tracked datasets"
 
     def get_metadata(self):
-        # TODO
-        pass
+        return self._metadata
 
     def test_dataloader(self) -> List[DataLoader]:
-        # TODO
-        return []
+        """Returns a test dataloader for each processed dataset"""
+        return [
+            DataLoader(
+                dataset,
+                batch_size=self.cfg.batch_size,
+                sampler=SequentialSampler(dataset),
+            )
+            for dataset in self._processed_datasets
+        ]
 
     def _load_dataset(self, dataset_name):
         # parses dataset name
