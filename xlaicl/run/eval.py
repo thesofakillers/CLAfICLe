a5a6832b0ca99103aeba0aa1ca554a32c79ebac2
Giulio Starace <giulio.starace@gmail.com>
2022-09-16 14:11:08 +0200

    refine eval.py flow

--- xlaicl/run/eval.py
+++ xlaicl/run/eval.py
@@ -12,17 +12,17 @@ def main(cfg: DictConfig):
     print(OmegaConf.to_yaml(cfg))
     model = Wrapper.load_from_checkpoint(cfg.checkpoint_path)
     # separate benchmarks by language
-    benchmarks = []
+    bmark_by_lang = {}
     langs = ["en", "de", "fr"]
     lang_flags = {lang: cfg[lang] for lang in langs}
     assert any(lang_flags.values()), "At least one of en, de, fr must be True"
     for lang, flag in lang_flags.items():
         if flag is True:
             benchmark = BenchmarkDataModule(config=cfg.benchmark, lang=lang)
-            benchmarks.append(benchmark)
+            bmark_by_lang[lang] = benchmark
     trainer = pl.Trainer(cfg.trainer)
-    for benchmark in benchmarks:
-        # so that the model knows names and metrics of dataloaders
+    for _lang, benchmark in bmark_by_lang.items():
+        # so that the model knows names and metrics of dataloaders before testing
         model.set_benchmark_metadata(benchmark.get_metadata())
         trainer.test(model, datamodule=benchmark)
 
