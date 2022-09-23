0f7d9903dc19289291b87c4ca403eec30c6ea4b3
Giulio Starace <giulio.starace@gmail.com>
2022-09-23 18:13:59 +0200

    start working on xcsr, run into bug

---
+++ claficle/data/process/xcsr.py
@@ -0,0 +1,23 @@
+from claficle.data.process.utils import ProcessHelper
+
+
+class XCSRHelper(ProcessHelper):
+    @staticmethod
+    def language_available(dataset_name, lang):
+        dataset_lang = dataset_name[-2:]
+        if dataset_lang == lang:
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def get_k_source(dataset, lang):
+        return dataset["validation"]
+
+    @staticmethod
+    def get_test_split(dataset, lang):
+        return dataset["test"]
+
+    @staticmethod
+    def get_options(example):
+
