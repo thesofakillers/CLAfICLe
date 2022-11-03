b15d11a8c214bec4f1b4876315b1f3e47e35da0f
Giulio Starace <giulio.starace@gmail.com>
2022-11-03 18:05:02 +0100

    skeleton of evalwrapper

--- claficle/models/evalwrapper.py
+++ claficle/models/evalwrapper.py
@@ -1,32 +1,30 @@
 import pytorch_lightning as pl
-import torch
-from transformers import AutoModelForCausalLM
 from omegaconf import DictConfig
+from claficle.models.utils import NAME_TO_CLASS
 
 
 class EvalWrapper(pl.LightningModule):
     # todo
     def __init__(self, cfg: DictConfig):
         super().__init__()
-        self.cfg = cfg
+        self.save_hyperparameters(cfg)
+        self._set_evaluand()
 
-    def load_checkpoint(self, checkpoint_path):
-        # todo
-        state_dict = torch.load(checkpoint_path)
-        self.causal_lm = AutoModelForCausalLM.from_pretrained(
-            self.cfg.causalLM_variant, state_dict=state_dict
+    def _set_evaluand(self):
+        """returns an instance the model we wish to evaluate"""
+        Evaluand_Class: pl.LightningModule = NAME_TO_CLASS[self.hparams.name]
+        self.evaluand = Evaluand_Class.load_from_checkpoint(
+            self.hparams.checkpoint_path
         )
-
-    def configure_approach(self):
-        # todo
-        pass
-
-    def forward(self, x):
-        # todo
-        pass
 
     def test_step(self, batch, batch_idx, dataloader_idx):
         # todo
+        # get data (batch)
+        # pre-collate processing (defined in evaluand)
+        # collate
+        # get log-likelihood from evaluand for each option completing the input
+        # evaluate (max log-likelihood = prediction -> compare to label)
+        # log
         pass
 
     def set_benchmark_metadata(self, metadata):
