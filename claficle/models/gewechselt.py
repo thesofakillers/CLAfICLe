6903bdd9c1de38c2a0c18849116634ac08b02885
Giulio Starace <giulio.starace@gmail.com>
2022-12-04 11:17:13 +0100

    integrate new initialize API

--- claficle/models/gewechselt.py
+++ claficle/models/gewechselt.py
@@ -14,7 +14,12 @@ class Gewechselt(BaseModel):
 
     def __init__(self, config: DictConfig):
         super().__init__(config)
-        # TODO
+
+    def initialize(self, config: DictConfig) -> AutoTokenizer:
+        """Applies WECHSEL initialization"""
+        source_tokenizer = AutoTokenizer.from_pretrained(config.causalLM_variant)
+        lm = AutoModelForCausalLM.from_pretrained(config.causalLM_variant)
+        # TODO: the rest of the wechsel tutorial
 
     def run_causal_model(self, input_ids: Tensor, attention_mask: Tensor) -> Tensor:
         # TODO
