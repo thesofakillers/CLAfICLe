56079a6324efc8953d0fa642ca3815345dcc14b7
Giulio Starace <giulio.starace@gmail.com>
2022-12-06 21:04:46 +0100

    integrate use of oscar datamodule

--- claficle/models/gewechselt.py
+++ claficle/models/gewechselt.py
@@ -1,13 +1,15 @@
 """
 A Gewechselt model: A model to which WECHSEL is applied
 """
-from typing import Tuple
+from typing import Tuple, Dict
 
 from omegaconf import DictConfig
 from wechsel import WECHSEL, load_embeddings
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from torch import Tensor
 import torch
+import hydra
+import datasets
 
 from claficle.models.base import BaseModel
 
@@ -23,17 +25,20 @@ class Gewechselt(BaseModel):
     Model initialized using WECHSEL (Minixhofer et al. 2022)
     """
 
-    def __init__(self, config: DictConfig):
-        super().__init__(config)
+    def __init__(self, config: DictConfig, target_data: datasets.arrow_dataset.Dataset):
+        super().__init__(config, target_data=target_data)
 
     def custom_init(
-        self, tokenizer, lm, config: DictConfig
+        self,
+        tokenizer,
+        lm,
+        config: DictConfig,
+        target_data: datasets.arrow_dataset.Dataset,  # this is a kwarg
     ) -> Tuple[AutoTokenizer, AutoModelForCausalLM]:
         """Applies WECHSEL initialization"""
+        print("Training target tokenizer...")
         target_tokenizer = tokenizer.train_new_from_iterator(
-            # TODO: datamodule for 4GB of OSCAR
-            # NOTE: we may need a bit more than 4GB since we're using gpt2-large
-            load_dataset("oscar", "unshuffled_deduplicated_sw", split="train")["text"],
+            target_data["text"],
             vocab_size=len(tokenizer),
         )
         wechsel = WECHSEL(
@@ -41,15 +46,48 @@ class Gewechselt(BaseModel):
             load_embeddings(config.target_lang),
             bilingual_dictionary=langcode_to_lang[config.target_lang],
         )
+        print("Generating target embeddings...")
         target_embeddings, info = wechsel.apply(
             tokenizer,
             target_tokenizer,
             lm.get_input_embeddings().weight.detach().numpy(),
         )
+        print("Replacing source embeddings with target embeddings...")
         lm.get_input_embeddings().weight.data = torch.from_numpy(target_embeddings)
+        print("Done.")
 
         return target_tokenizer, lm
 
     def run_causal_model(self, input_ids: Tensor, attention_mask: Tensor) -> Tensor:
         # TODO
         raise NotImplementedError
+
+
+@hydra.main(version_base=None, config_path="../conf/model/", config_name="base_wechsel")
+def main(cfg: DictConfig):
+    """downloads and processes the data for benchmark for each of the available languages"""
+    from claficle.data.oscar import OSCARDataModule
+    import yaml
+    from yaml.loader import SafeLoader
+
+    print(cfg)
+    cfg.name = "gewechselt"
+    cfg.causalLM_variant = "distilgpt2"
+    cfg.target_lang = "fr"
+
+    # load separate data config from ../conf/data/oscar_base.yaml
+    with open("claficle/conf/data/oscar_base.yaml", "r") as f:
+        data_cfg = yaml.load(f, Loader=SafeLoader)
+    data_cfg = DictConfig(data_cfg)
+
+    data_cfg.sample_size_mb = 1024
+
+    oscar_fr = OSCARDataModule(data_cfg, cfg.target_lang, 1)
+    oscar_fr.prepare_data()
+    oscar_fr.setup()
+
+    model = Gewechselt(cfg, oscar_fr.train_dataset)
+
+
+if __name__ == "__main__":
+    main()
