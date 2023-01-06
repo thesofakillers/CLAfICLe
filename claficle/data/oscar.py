4f30f475a82579fe63101b3746a6031c9b41ed76
Giulio Starace <26286291+thesofakillers@users.noreply.github.com>
2023-01-06 19:01:11 +0100

    set up memory profiling dataloader and collate function

--- claficle/data/oscar.py
+++ claficle/data/oscar.py
@@ -1,18 +1,22 @@
 import os
 from pathlib import Path
 import json
-from typing import Any, Dict, Generator, Optional
+from typing import Any, Dict, Generator, Optional, List
 
 import hydra
 from omegaconf import DictConfig
 import pytorch_lightning as pl
 import datasets
+import transformers
 from tqdm import tqdm
+import torch
 
 
 class OSCARDataModule(pl.LightningDataModule):
     """
     PL DataModule responsible for OSCAR dataloading
+
+    Note: run `set_tokenizer(tokenizer)` before running self.setup()
     """
 
     def __init__(self, config: DictConfig, lang, seed):
@@ -37,7 +41,9 @@ class OSCARDataModule(pl.LightningDataModule):
             Path(self.save_dir).mkdir(parents=True, exist_ok=True)
             if self.cfg.sample_size_mb is None:
                 dataset: datasets.arrow_dataset.Dataset = datasets.load_dataset(
-                    "oscar", f"unshuffled_deduplicated_{self.lang}", split="train"
+                    "oscar-corpus/OSCAR-2201",
+                    f"unshuffled_deduplicated_{self.lang}",
+                    split="train",
                 )
                 # manually split into train and test and save to disk
                 dataset = dataset.train_test_split(test_size=self.cfg.val_percent)
@@ -76,7 +82,57 @@ class OSCARDataModule(pl.LightningDataModule):
             self.val_dataset = datasets.load_dataset(
                 self.save_dir, split="validation", cache_dir=self.save_dir
             )
+        if stage == "profile_memory":
+            # 100k sequences of max length of random tokens
+            self.profile_mem_dset_tokens = datasets.Dataset.from_dict(
+                {
+                    "input_ids": torch.randint(
+                        0, len(self.tokenizer), size=(1e5, self.max_seq_length)
+                    ),
+                    "attention_mask": torch.ones((1e5, self.max_seq_length), dtype=int),
+                }
+            )
+            # all the same length so no need for sorting :)
+        if stage == "profile_time":
+            # TODO subset of train set
+            pass
+
         self.is_setup = True
+
+    def memory_profile_dataloader(self) -> torch.utils.data.DataLoader:
+        return torch.utils.data.DataLoader(
+            self.profile_mem_dset_tokens,
+            batch_size=self.cfg.batch_size,
+            collate_fn=self.collate_fn,
+            num_workers=self.cfg.num_workers,
+            pin_memory=True,
+        )
+
+    def set_tokenizer(self, tokenizer):
+        self.tokenizer = tokenizer
+        self.tokenizer.truncation_side = "left"
+        self.pad_token_id = tokenizer.convert_tokens_to_ids(
+            tokenizer.special_tokens_map["pad_token"]
+        )
+        self.max_seq_length = min(1024, tokenizer.model_max_length)
+        self.vocab_size = len(self.tokenizer)
+
+    def collate_fn(
+        self, features: List[Dict[str, torch.Tensor]]
+    ) -> Dict[str, torch.Tensor]:
+        """
+        Converts a list of dictionaries of tensors into a dictionary of tensors
+        Padding has already been applied to batches of different sizes by the tokenizer
+        Here we remove redundant padding
+        """
+        # converting to dict of tensors
+        features = transformers.default_data_collator(features)
+        # determining last relevant token
+        final_idx = features["attention_mask"].any(dim=0).nonzero()[-1]
+        # removing anything after this token
+        features["input_ids"] = features["input_ids"][:, : final_idx + 1]
+        features["attention_mask"] = features["attention_mask"][:, : final_idx + 1]
+        return features
 
 
 def datastream_to_file(
