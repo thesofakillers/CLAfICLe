cd5245bbae2c7dae05aff7d01e3c491b55d5d0e3
Giulio Starace <26286291+thesofakillers@users.noreply.github.com>
2023-01-09 12:08:23 +0100

    better docs

--- claficle/utils/general.py
+++ claficle/utils/general.py
@@ -9,6 +9,11 @@ from claficle.models.base import BaseModel
 
 
 def run_script_preamble(cfg: DictConfig) -> Tuple[BaseModel, DictConfig]:
+    """
+    sets the seed
+    parses the model name and initializes/loads the model
+    modifies the cfg accordingly
+    """
     pl.seed_everything(cfg.seed, workers=True)
     print(OmegaConf.to_yaml(cfg))
     ModelClass: BaseModel = NAME_TO_CLASS[cfg.model.name]
@@ -16,10 +21,10 @@ def run_script_preamble(cfg: DictConfig) -> Tuple[BaseModel, DictConfig]:
     if cfg.model.pl_checkpoint:
         model = ModelClass.load_from_checkpoint(cfg.model.pl_checkpoint)
     else:
-        model = ModelClass(cfg.model)
-    # get possible additional preprocessing from model and set in benchmark cfg
+        model = ModelClass(cfg.model)  # this loads non-pl checkpoints internally
+    # get possible additional preprocessing from model and set in data cfg
     cfg.data.extra_proc_fn = cfg.model.extra_proc_fn
-    # overwrite data cfg seed with eval cfg seed
+    # overwrite data cfg seed with run script seed
     cfg.data.seed = cfg.seed
 
     return model, cfg
