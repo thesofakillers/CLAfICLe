853bd6b66258b0290c4006b01f4650307b864dad
Giulio Starace <26286291+thesofakillers@users.noreply.github.com>
2023-01-17 10:33:21 +0100

    train script and conf files

---
+++ claficle/run/train.py
@@ -0,0 +1,63 @@
+"""Train our models"""
+import os
+
+from omegaconf import DictConfig, OmegaConf
+import hydra
+import transformers
+import pytorch_lightning as pl
+
+from claficle.models.base import BaseModel
+from claficle.utils.general import run_script_preamble
+from claficle.data.oscar import OSCARDataModule
+
+
+@hydra.main(version_base=None, config_path="../conf", config_name="train")
+def main(cfg: DictConfig):
+    if cfg.model.pl_checkpoint is None:
+        raise ValueError("Must provide a (init) PL checkpoint to train")
+    # sets seed, parses model name
+    model: BaseModel
+    model, cfg = run_script_preamble(cfg)
+
+    # data
+    oscar = OSCARDataModule(config=cfg.data, lang=cfg.model.target_lang, seed=cfg.seed)
+    if cfg.tokenizer_name is None:
+        if cfg.model.target_lang != "en":
+            raise ValueError("Must provide a tokenizer path for non-English models")
+        tokenizer = transformers.AutoTokenizer.from_pretrained(
+            cfg.model.causalLM_variant
+        )
+    else:
+        tokenizer_path = os.path.join("checkpoints", "tokenizers", cfg.tokenizer_name)
+        tokenizer = transformers.AutoTokenizer.from_pretrained(tokenizer_path)
+
+    oscar.prepare_data()
+    oscar.set_tokenizer(tokenizer)
+    oscar.setup()
+
+    # trainer
+    log_save_dir = os.path.join(
+        cfg.trainer.log_dir, cfg.model.name, f"seed_{cfg.seed}", model.cfg.target_lang
+    )
+    os.makedirs(log_save_dir, exist_ok=True)
+    script_host = "slurm" if "SLURM_JOB_ID" in os.environ else "local"
+    logger = pl.loggers.WandbLogger(
+        save_dir=log_save_dir,
+        entity="giulio-uva",
+        project="claficle",
+        job_type="train",
+        mode="disabled" if cfg.trainer.disable_wandb else "online",
+        group=script_host,
+        config=OmegaConf.to_container(cfg, resolve=True, throw_on_missing=True),
+    )
+
+    trainer = pl.Trainer(
+        max_epochs=1,
+        logger=logger,
+        enable_progress_bar=cfg.trainer.progress_bar,
+        accelerator=cfg.trainer.accelerator,
+        devices=cfg.trainer.devices,
+    )
+    model.train_mode = cfg.trainer.train_mode
+
+    trainer.fit(model, oscar)
