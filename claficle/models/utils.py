a3772f95e843b31f59d00e5a38c6d71c785acd40
Giulio Starace <26286291+thesofakillers@users.noreply.github.com>
2023-01-18 14:12:22 +0100

    name to class doesn't handle debug, and was missing gewechselt

--- claficle/models/utils.py
+++ claficle/models/utils.py
@@ -1,4 +1,9 @@
 from claficle.models.sandwich import Sandwich
 from claficle.models.plain_gpt2 import PlainGPT2
+from claficle.models.gewechselt import Gewechselt
 
-NAME_TO_CLASS = {"sandwich": Sandwich, "plain_gpt2": PlainGPT2}
+NAME_TO_CLASS = {
+    "sandwich": Sandwich,
+    "plain_gpt2": PlainGPT2,
+    "gewechselt": Gewechselt,
+}
