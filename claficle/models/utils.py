d9362ab9fcc790fa52db4cff916fde5adfda03d3
Giulio Starace <26286291+thesofakillers@users.noreply.github.com>
2023-01-26 14:48:32 +0100

    add 'vessel' name class

--- claficle/models/utils.py
+++ claficle/models/utils.py
@@ -1,9 +1,11 @@
 from claficle.models.sandwich import Sandwich
 from claficle.models.plain_gpt2 import PlainGPT2
 from claficle.models.gewechselt import Gewechselt
+from claficle.models.vessel import Vessel
 
 NAME_TO_CLASS = {
     "sandwich": Sandwich,
     "plain_gpt2": PlainGPT2,
     "gewechselt": Gewechselt,
+    "vessel": Vessel
 }
