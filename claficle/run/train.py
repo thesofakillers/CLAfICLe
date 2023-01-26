21fedf2f07c5c986f0b1f17456d238bd6f4f4246
Giulio Starace <26286291+thesofakillers@users.noreply.github.com>
2023-01-26 13:41:00 +0100

    Revert "adapt training script to work with distillation"
    This reverts commit 4db455db825e86cd7cc376549b92074c12ca4a55.

--- claficle/run/train.py
+++ claficle/run/train.py
@@ -13,16 +13,11 @@ from claficle.data.oscar import OSCARDataModule
 
 @hydra.main(version_base=None, config_path="../conf", config_name="train")
 def main(cfg: DictConfig):
-    """
-    Set --config_name=distil to run distillation instead of training
-    """
     if cfg.model.pl_checkpoint is None:
         raise ValueError("Must provide a (init) PL checkpoint to train")
     # sets seed, parses model name
     model: BaseModel
     model, cfg = run_script_preamble(cfg)
-    if cfg.post_init:
-        model.post_init()
 
     # data
     oscar = OSCARDataModule(config=cfg.data, lang=cfg.model.target_lang, seed=cfg.seed)
@@ -40,7 +35,7 @@ def main(cfg: DictConfig):
 
     oscar.prepare_data()
     oscar.set_tokenizer(tokenizer)
-    oscar.setup(cfg.stage)
+    oscar.setup("debug" if cfg.debug else None)
 
     # trainer
     log_save_dir = os.path.join(
