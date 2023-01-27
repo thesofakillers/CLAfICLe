4d9b71a51cdeb8dd7c65887b629242da512b65b6
Giulio Starace <26286291+thesofakillers@users.noreply.github.com>
2023-01-27 15:49:09 +0100

    being rewrite of eval script

--- claficle/run/eval.py
+++ claficle/run/eval.py
@@ -3,7 +3,7 @@ import os
 
 import pytorch_lightning as pl
 import hydra
-from omegaconf import DictConfig
+from omegaconf import DictConfig, OmegaConf
 
 from claficle.data.benchmark import BenchmarkDataModule
 from claficle.models.base import BaseModel
@@ -16,47 +16,41 @@ def main(cfg: DictConfig):
     model: BaseModel
     model, cfg = run_script_preamble(cfg)
 
+    lang = cfg.lang
     # separate benchmarks by language
-    bmark_by_lang = {}
-    langs = ["en", "de", "fr"]
-    lang_flags = {lang: cfg[lang] for lang in langs}
-    assert any(lang_flags.values()), "At least one of en, de, fr must be True"
-    for lang, flag in lang_flags.items():
-        if flag is True:
-            print(f"Setting up data for evaluation in {lang}...")
-            bmark_by_lang[lang] = BenchmarkDataModule(config=cfg.data, lang=lang)
-            bmark_by_lang[lang].prepare_data()
-            bmark_by_lang[lang].setup()
-    for lang in langs:
-        if lang in bmark_by_lang:
-            benchmark = bmark_by_lang[lang]
-        else:
-            continue
-        # so that the model knows names and metrics of dataloaders before testing
-        model.set_benchmark_metadata(benchmark.get_metadata())
-        benchmark.set_tokenizer(model.tokenizer)
-        if cfg.data.extra_proc_fn is None:
-            benchmark.set_pre_collate_fn(model.pre_collate)
+    benchmark = BenchmarkDataModule(config=cfg.data, lang=lang)
+    benchmark.prepare_data()
+    benchmark.setup()
+    # so that the model knows names and metrics of dataloaders before testing
+    model.set_benchmark_metadata(benchmark.get_metadata())
 
-        # set up pl trainer (tester)
-        log_save_dir = os.path.join(
-            cfg.trainer.log_dir, cfg.model.name, f"seed_{cfg.seed}", lang
-        )
-        os.makedirs(log_save_dir, exist_ok=True)
-        logger = pl.loggers.WandbLogger(
-            save_dir=log_save_dir,
-            job_type="eval",
-            project="claficle",
-            entity="giulio-uva",
-        )
-        trainer = pl.Trainer(
-            logger=logger,
-            enable_progress_bar=cfg.trainer.progress_bar,
-            accelerator=cfg.trainer.accelerator,
-            devices=cfg.trainer.devices,
-        )
-        print(f"Evaluating in {lang}...")
-        trainer.test(model, datamodule=benchmark)
+    # TODO: handle tokenizer
+    benchmark.set_tokenizer(model.tokenizer)
+
+    # set up pl trainer (tester)
+    log_save_dir = os.path.join(
+        cfg.trainer.log_dir, cfg.model.name, f"seed_{cfg.seed}", lang
+    )
+    os.makedirs(log_save_dir, exist_ok=True)
+    script_host = "slurm" if "SLURM_JOB_ID" in os.environ else "local"
+    logger = pl.loggers.WandbLogger(
+        save_dir=log_save_dir,
+        project="claficle",
+        entity="giulio-uva",
+        job_type="eval",
+        mode="disabled" if cfg.disable_wandb else "online",
+        group=script_host,
+        config=OmegaConf.to_container(cfg, resolve=True, throw_on_missing=True),
+        log_model=False,  # don't log or upload artifacts
+    )
+    trainer = pl.Trainer(
+        logger=logger,
+        enable_progress_bar=cfg.trainer.progress_bar,
+        accelerator=cfg.trainer.accelerator,
+        devices=cfg.trainer.devices,
+    )
+    print(f"Evaluating in {lang}...")
+    trainer.test(model, datamodule=benchmark)
     print("Done.")
 
 
